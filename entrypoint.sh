e2e1ff3805d4edd94a96d4e53d42c2ec12893947
flysoloing <flysoloing@gmail.com>
2020-07-23 20:10:38 +0800

    test

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,33 +7,43 @@ time=$(date)
 echo "--------------------------"
 echo ::set-output name=time::$time
 
+testV=$1
+
+echo $testV
+
+echo "the first param is $testV" 
+
+hugo version
+
+git --version
+
 # https://github.com/<USERNAME>/<PROJECT>.git
-source_url=$1
+#source_url=$1
 # <PROJECT>
-source_dir=""
+#source_dir=""
 # https://github.com/<USERNAME>/<USERNAME>.github.io.git
-target_url=$2
+#target_url=$2
 # https://<USERNAME>.github.io
-baseURL=$222
+#baseURL=$222
 
-cd /home/
+#cd /home/
 
-git clone $source_url
+#git clone $source_url
 
-cd $source_dir
+#cd $source_dir
 
-hugo -D
+#hugo -D
 
-git submodule add -b master $target_url public
+#git submodule add -b master $target_url public
 
-cd public
+#cd public
 
-git add .
+#git add .
 
-msg="rebuilding site $(date)"
-if [ -n "$*" ]; then
-	msg="$*"
-fi
-git commit -m "$msg"
+#msg="rebuilding site $(date)"
+#if [ -n "$*" ]; then
+#	msg="$*"
+#fi
+#git commit -m "$msg"
 
-git push origin master
+#git push origin master
