b849bebb38bc7bb0011b39bc48c9bee61a6eb496
flysoloing <flysoloing@gmail.com>
2020-08-02 15:59:16 +0800

    refact

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,22 +8,18 @@ git --version
 
 #设置git支持中文文件名的处理
 git config --global core.quotepath false
+git config user.name "${GITHUB_ACTOR}"
+git config user.email "${GITHUB_ACTOR}@users.noreply.github.com"
 
+theme_repo_url=$1
+source_repo_url=$2
+target_repo_url=$3
+config_file_url=$4
+base_url=$5
+title=$6
+language_code=$7
+theme=$8
 
-echo "GITHUB ACTOR: ${GITHUB_ACTOR}"
-
-
-hugo_version=$1
-theme_repo_url=$2
-source_repo_url=$3
-target_repo_url=$4
-config_file_url=$5
-base_url=$6
-title=$7
-language_code=$9
-theme=$9
-
-echo "hugo version: $hugo_version"
 echo "theme repo url: $theme_repo_url"
 echo "source repo url: $source_repo_url"
 echo "target repo url: $target_repo_url"
@@ -33,24 +29,6 @@ echo "title: $title"
 echo "language_code: $language_code"
 echo "theme: $theme"
 
-workspace_dir="workspace"
-echo "workspace dir: $workspace_dir"
-
-workspace_path=~/$workspace_dir
-echo "workspace path: $workspace_path"
-
-mkdir $workspace_path
-cd $workspace_path
-pwd
-
-site_dir="xxxxxx"
-source_dir=${source_repo_url##*/}
-target_dir=${target_repo_url##*/}
-
-echo "site dir: $site_dir"
-echo "source dir: $source_dir"
-echo "target dir: $target_dir"
-
 if [ -z "$source_repo_url" ]; then
     echo "source repo url is none, exit"
 	exit
@@ -59,6 +37,24 @@ if [ -z "$target_repo_url" ]; then
     echo "target repo url is none, exit"
 	exit
 fi
+
+workspace_dir="workspace"
+site_dir="xxxxxx"
+source_dir=${source_repo_url##*/}
+target_dir=${target_repo_url##*/}
+
+echo "workspace dir: $workspace_dir"
+echo "site dir: $site_dir"
+echo "source dir: $source_dir"
+echo "target dir: $target_dir"
+
+workspace_path=~/$workspace_dir
+echo "workspace path: $workspace_path"
+
+mkdir $workspace_path
+cd $workspace_path
+pwd
+
 git clone $source_repo_url
 git clone $target_repo_url
 
@@ -70,11 +66,6 @@ cd $workspace_path/$source_dir
 pwd
 ls -al
 cp -r *.md $workspace_path/$site_dir/content
-#检查是否包含front matter，若没有，则加上，事例如下：
-#---
-#title: "title"
-#date: "2020-07-30T20:20:20Z"
-#---
 
 #进行hugo部署前的配置
 cd $workspace_path/$site_dir
@@ -85,11 +76,11 @@ ls -al
 if [ -z "$config_file_url" ]; then
     echo "config file url is none, use basic config params"
     #替换config.toml中对应的几个字段，baseURL
-    sed -i '/baseURL/ c baseURL = \"`$base_url`\"' config.toml
-    sed -i '/languageCode/ c languageCode = \"$language_code\"' config.toml
-    sed -i '/title/ c title = \"$title\"' config.toml
-    #sed -i '/theme/ c theme = \"$theme\"' config.toml
-	echo "theme = \"`$theme`\"" >> config.toml
+    sed -i "/baseURL/ c baseURL = \"${base_url}\"" config.toml
+    sed -i "/languageCode/ c languageCode = \"$language_code\"" config.toml
+    sed -i "/title/ c title = \"$title\"" config.toml
+    #sed -i "/theme/ c theme = \"$theme\"" config.toml
+	echo "theme = \"$theme\"" >> config.toml
 else
     echo "replace default config.toml"
     wget $config_file_url -O config.toml
@@ -107,6 +98,11 @@ ls -al
 
 #为每个md文件增加头部信息，如title，
 cd $workspace_path/$site_dir/content
+#检查是否包含front matter，若没有，则加上，事例如下：
+#---
+#title: "title"
+#date: "2020-07-30T20:20:20Z"
+#---
 
 #以上设置完毕，开始hugo部署
 cd $workspace_path/$site_dir
@@ -125,7 +121,10 @@ git rm $(git ls-files -d)
 git status
 git commit -m "remove old files"
 git status
-git push origin master
+
+target_repo_url_with_token=`echo $target_repo_url | sed "s/github/${GITHUB_TOKEN}@&/"`
+echo $target_repo_url_with_token
+git push -f -q $target_repo_url_with_token master
 git status
 
 #将public目录内容拷贝到target目录
