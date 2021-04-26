9341272e388fb7101f298c8685b9ed6e5ed2fcf9
flysoloing <flysoloing@gmail.com>
2021-04-26 15:24:06 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -83,6 +83,7 @@ git clone $target_repo_url_with_token $target_dir
 #初始化站点结构
 logger "create new site: $site_dir"
 hugo new site $site_dir
+echo "theme = \"xxx\"" >> config.toml
 
 logger "there are three directories in the workspace"
 pwd && ls -l
@@ -90,8 +91,6 @@ pwd && ls -l
 #进行hugo部署前的配置
 cd $workspace_path/$site_dir
 pwd && ls -al
-
-echo "theme = \"qiuqiu\"" >> config.toml
 
 #设置config.toml，如果config_file_url参数为空，则用base_url等基本参数进行配置，否则先替换后再进行基本配置，此处可先做非空判断，TODO
 if [ -z "$config_file_url" ]; then
